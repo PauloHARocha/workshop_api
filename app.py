f67c69ff6d55e1c772b7030f5b9292a105510c09
pauloharocha <ph.alcantara.rocha@gmail.com>
2021-05-18 14:23:56 -0300

    Adding POST produto

--- app.py
+++ app.py
@@ -1,13 +1,32 @@
 import os, json
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/produto', methods=['GET'])
-def produto():
+@app.route('/produto', methods=['GET', 'POST'])
+def produtos():
     with open('produtos.json') as f:
         data = json.load(f)
-    return data, 200
+
+    if request.method == 'POST':
+        content = request.json
+        data['produtos'].append(content)
+        with open('produtos.json', 'w') as f:
+            json.dump(data, f)
+        return content, 200
+    else:
+        
+        return data, 200
+
+
+@app.route('/produto/<id>', methods=['GET'])
+def produto(id):
+    with open('produtos.json') as f:
+        data = json.load(f)
+    for p in data['produtos']:
+        if p['id'] == int(id):
+            return p
+    return 'Não foi encontrado', 404
 
 
 
