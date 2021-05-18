3868dcad3977efa1a7f36e70308b0c5f2e116ebb
pauloharocha <ph.alcantara.rocha@gmail.com>
2021-05-18 14:23:37 -0300

    Adding endpoint produto GET

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-import os
+import os, json
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/home')
-def hello_world():
-    return 'Hello World'
+@app.route('/produto', methods=['GET'])
+def produto():
+    with open('produtos.json') as f:
+        data = json.load(f)
+    return data, 200
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=os.getenv('PORT'))
